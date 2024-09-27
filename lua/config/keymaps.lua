23907ba5d7345fb21a9d7f89ce9a696e0d57f11b
joperez23 <joperezd23@gmail.com>
2024-09-27 16:07:19 -0400

    Keymap kulala

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ vim.keymap.set(
   -- vim.api.nvim_buf_set_keymap(
   --0,
   "n",
-  "<CR>",
+  "<c-l>",
   "<cmd>lua require('kulala').run()<cr>",
   { noremap = true, silent = true, desc = "Execute the request" }
 )
